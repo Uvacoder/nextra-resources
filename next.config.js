5f5545180d032afca6aadd3fda057367eaedb193
Kristoff <chrystalio.k@outlook.com>
2022-10-15 13:22:40 +0700

    Remove on footer edit github

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
   unstable_staticImage: true,
+  editLink: { component: null },
 })
 module.exports = withNextra()
